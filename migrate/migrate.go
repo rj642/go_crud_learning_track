e8f3e34c068410f6936a3619b7ea623b931d5dff
Rahul Jha <rahulj.rj642@gmail.com>
2023-04-09 10:17:28 +0530

    update: comments

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -14,3 +14,8 @@ func main() {
 	// To create a table, use this command written below
 	initializers.DB.AutoMigrate(&models.Post{})
 }
+
+/**
+run this migrate.go file to create an empty table with the desired model class.
+go run migrate.go -> It will create an empty table with schema as post class
+**/
